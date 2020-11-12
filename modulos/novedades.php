979a1c9947a7cf247d066d72f070ad46868e30c9
Gonzalo Tudela <gondbt@gmail.com>
2020-11-12 16:05:14 +0100

    mejoras grid y diseño test

--- modulos/novedades.php
+++ modulos/novedades.php
@@ -7,27 +7,105 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Novedades</title>
     <link rel="icon" href="../img/favicon.png">
-    <link rel="stylesheet" href="../css/novedades.css">
+    <link rel="stylesheet" href="../css/general.css">
     <script type="text/javascript" src="../librerias/funcionesJS.js" async></script>
 </head>
 <body>
-<div id="fondo">
-    <header>
-        <h1>NOVEDADES</h1>
+<div class="grid-container">
+    <header class="altura2 sombra1">
+        <h1 class="txt0">NOVEDADES</h1>
     </header>
     <main>
-        <div class="area_util">
-            <div class="card">
-                <h2>Titulo</h2>
-                <p>Texto</p>
-                <p class="disabled">Disabled</p>
-                <button class="button_active">BOTÓN</button>
+        <div class="scroll">
+            <div class="card altura1 sombra2">
+                <div class="izq altura2 sombra3">
+                    <h2 class="txt1">Titulo</h2>
+                    <p class="txt2">Cuerpo</p>
+                    <p class="txt3">Notas</p>
+                    <p class="txt-off">Texto disabled</p>
+                    <p class="txt-r0">Resalte0</p>
+                    <p class="txt-r1">Resalte1</p>
+                    <p class="txt-r2">Resalte2</p>
+                    <p class="txt-r3">Resalte3</p>
+                    <p class="txt-r-off">Resalte disabled</p>
+                    <div class="card altura3 sombra4">
+                        <h2 class="txt1">Titulo</h2>
+                        <p class="txt2">Cuerpo</p>
+                        <p class="txt3">Notas</p>
+                        <p class="txt-off">Texto disabled</p>
+                        <p class="txt-r0">Resalte0</p>
+                        <p class="txt-r1">Resalte1</p>
+                        <p class="txt-r2">Resalte2</p>
+                        <p class="txt-r3">Resalte3</p>
+                        <p class="txt-r-off">Resalte disabled</p>
+                    </div>
+                </div>
+                <div class="der altura2 sombra2">
+                    <h2 class="txt1">Titulo</h2>
+                    <p class="txt2">Cuerpo</p>
+                    <p class="txt3">Notas</p>
+                    <p class="txt-off">Texto disabled</p>
+                    <p class="txt-r0">Resalte0</p>
+                    <p class="txt-r1">Resalte1</p>
+                    <p class="txt-r2">Resalte2</p>
+                    <p class="txt-r3">Resalte3</p>
+                    <p class="txt-r-off">Resalte disabled</p>
+                </div>
             </div>
+            <div class="card altura1 sombra1">
+                <div class="izq altura2 sombra2">
+                    <h2 class="txt1">Titulo</h2>
+                    <p class="txt2">Cuerpo</p>
+                    <p class="txt3">Notas</p>
+                    <p class="txt-off">Texto disabled</p>
+                    <p class="txt-r0">Resalte0</p>
+                    <p class="txt-r1">Resalte1</p>
+                    <p class="txt-r2">Resalte2</p>
+                    <p class="txt-r3">Resalte3</p>
+                    <p class="txt-r-off">Resalte disabled</p>
+                </div>
+                <div class="der altura2 sombra2">
+                    <h2 class="txt1">Titulo</h2>
+                    <p class="txt2">Cuerpo</p>
+                    <p class="txt3">Notas</p>
+                    <p class="txt-off">Texto disabled</p>
+                    <p class="txt-r0">Resalte0</p>
+                    <p class="txt-r1">Resalte1</p>
+                    <p class="txt-r2">Resalte2</p>
+                    <p class="txt-r3">Resalte3</p>
+                    <p class="txt-r-off">Resalte disabled</p>
+                </div>
+            </div>
+            <div class="card altura1 sombra1">
+                <div class="izq altura2 sombra2">
+                    <h2 class="txt1">Titulo</h2>
+                    <p class="txt2">Cuerpo</p>
+                    <p class="txt3">Notas</p>
+                    <p class="txt-off">Texto disabled</p>
+                    <p class="txt-r0">Resalte0</p>
+                    <p class="txt-r1">Resalte1</p>
+                    <p class="txt-r2">Resalte2</p>
+                    <p class="txt-r3">Resalte3</p>
+                    <p class="txt-r-off">Resalte disabled</p>
+                </div>
+                <div class="der altura2 sombra2">
+                    <h2 class="txt1">Titulo</h2>
+                    <p class="txt2">Cuerpo</p>
+                    <p class="txt3">Notas</p>
+                    <p class="txt-off">Texto disabled</p>
+                    <p class="txt-r0">Resalte0</p>
+                    <p class="txt-r1">Resalte1</p>
+                    <p class="txt-r2">Resalte2</p>
+                    <p class="txt-r3">Resalte3</p>
+                    <p class="txt-r-off">Resalte disabled</p>
+                </div>
+            </div>
+            <div id="error_container altura1"></div>
         </div>
-        <div id="error_container"></div>
     </main>
-    <footer>
-        <p>Menú</p>
+    <footer class="altura2">
+        <button class="button sombra1">+</button>
+        <button class="button sombra1 disabled" disabled>-</button>
     </footer>
 </div>
 </body>
