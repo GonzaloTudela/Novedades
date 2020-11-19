15a89b536307b4a2c88d385b5773d10f7dfab6f1
Gonzalo Tudela <gondbt@gmail.com>
2020-11-19 22:55:51 +0100

    Ahora contiene la funcion que escribe el menú

--- librerias/funcionesPHP.php
+++ librerias/funcionesPHP.php
@@ -9,16 +9,19 @@ function debugFor($ip)
         error_reporting(0);
     }
 }
-function debugConsole($datos) {
-    echo "<script>console.log(".json_encode($datos).")</script>";
+
+function debugConsole($datos)
+{
+    echo "<script>console.log(" . json_encode($datos) . ")</script>";
 }
+
 // Recorro el array $keyNovedades y escribo el contenido en la pagina.
 function escribeNovedades(array $array)
 {
     if (empty($array)) {
         echo '<div class="msg">';
         echo file_get_contents("../img/robot.svg");
-        echo '<p>SIN NOVEDADES</p>';
+        echo '<p>SIN NOTICIAS</p>';
         echo '</div>';
     } else {
         foreach ($array as $keyNovedades) {
@@ -54,6 +57,97 @@ function escribeNovedades(array $array)
         }
     }
 
+}
+
+// Imprimo el menu utilizando un array con el ID, icono, link y texto descriptivo.
+function printMenu()
+{
+    // Iconos para el boton.
+    $iconAdd = '../img/add.svg';
+    $iconSearch = '../img/search.svg';
+    $iconNews = '../img/news.svg';
+    $iconRules = '../img/rules.svg';
+    $iconOffice = '../img/office.svg';
+    $iconAccount = '../img/account.svg';
+    $iconQuit = '../img/exit.svg';
+    // Links del boton.
+    $webAdd = '';
+    $webSearch = '';
+    $webNews = '../modulos/novedades.php';
+    $webRules = '../modulos/normas.php';
+    $webOffice = '';
+    $webAccount = '';
+    $webQuit = '';
+    // Array menu estandard, id, icono, link, texto.
+    $menu = array(
+        0 => array('add', $iconAdd, $webAdd, 'Nueva'),
+        1 => array('iconSearch', $iconSearch, $webSearch, 'Buscar'),
+        2 => array('news', $iconNews, $webNews, 'Novedades'),
+        3 => array('rules', $iconRules, $webRules, 'Normas'),
+        4 => array('account', $iconAccount, $webAccount, 'Cuenta'),
+        5 => array('quit', $iconQuit, $webQuit, 'EXIT'));
+    // Array menu admin
+    $menuAdmin = array(
+        0 => array('add', $iconAdd, $webAdd, 'Nueva'),
+        1 => array('iconSearch', $iconSearch, $webSearch, 'Buscar'),
+        2 => array('news', $iconNews, $webNews, 'Novedades'),
+        3 => array('rules', $iconRules, $webRules, 'Normas'),
+        4 => array('office', $iconOffice, $webOffice, 'Empresas'),
+        5 => array('account', $iconAccount, $webAccount, 'Cuenta'),
+        6 => array('quit', $iconQuit, $webQuit, 'EXIT'));
+    // RECUPERO EL NIVEL DEL USUARIO PARA COMPROBAR MAS ADELANTE
+    if (isset($_SESSION['nivel'])) {
+        $nivel = $_SESSION['nivel'];
+    } else {
+        echo '<script>console.log("Nivel no existe")</script>';
+        exit();
+    }
+    // SI EL USUARIO ES NORMAL
+    if ($nivel >= 0 && $nivel <= 998) {
+        foreach ($menu as $item) {
+            $id = $item[0];
+            $icon = $item[1];
+            $web = $item[2];
+//            $texto = $item[3];
+            // EN CASO DE QUE EL DATO SEA EL DEL BOTON QUIT NO ESCRIBIMOS EL TAG <A> PARA EVITAR SALIR SIN MOSTRAR
+            // EL MENSAJE DE CONFIRMACION.
+            echo '<button id="' . $id . '" class="menuButton ">';
+            if ($item[0] !== 'quit') {
+                echo '<a href="' . $web . '">';
+            }
+//            echo $texto;
+            // EN CASO DE QUE EL DATO SEA EL DEL BOTON QUIT NO ESCRIBIMOS EL TAG <A> PARA EVITAR SALIR SIN MOSTRAR
+            // EL MENSAJE DE CONFIRMACION.
+            echo file_get_contents($icon);
+            if ($item[0] !== 'quit') {
+                echo '</a>';
+            }
+//            echo '<p class="hint">' . $texto . '</p>';
+            echo '</button>';
+//            echo '</button></a>';
+        }
+    } elseif ($nivel === 999) {
+        foreach ($menuAdmin as $item) {
+            $id = $item[0];
+            $icon = $item[1];
+            $web = $item[2];
+//            $texto = $item[3];
+            echo '<button id="' . $id . '" class="menuButton ">';
+            if ($item[0] !== 'quit') {
+                echo '<a href="' . $web . '">';
+            }
+//            echo $texto;
+            echo file_get_contents($icon);
+            if ($item[0] !== 'quit') {
+                echo '</a>';
+            }
+//            echo '<p class="hint">' . $texto . '</p>';
+            echo '</button>';
+//            echo '</button></a>';
+        }
+    } else {
+        echo '<script>console.log ("El nivel esta fuera de los limites!")</script>';
+    }
 }
 
 function resetAllSessions()
