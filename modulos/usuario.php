2d70b380fc62ad1b66d787615a1b4992cca9444f
Gonzalo Tudela <gondbt@gmail.com>
2020-11-25 11:53:40 +0100

    commit inicial

---
+++ modulos/usuario.php
@@ -0,0 +1,162 @@
+<?php
+require_once('../librerias/errores.php');
+require_once('../librerias/consultas.php');
+require_once('../librerias/funcionesPHP.php');
+//debugFor("79.152.7.228");
+
+//region DATOS RECOGIDOS EN LOGIN - SI NO ESTÁN, ACCESO NO AUTENTIFICADO -> LOGIN.PHP.
+session_start();
+if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], $_SESSION['apellido2'],
+    $_SESSION['estado_usu'], $_SESSION['$empresas'])) {
+    $id_usuario = $_SESSION['id_usuario'];
+    $nivel = $_SESSION['nivel'];
+    $nombre = $_SESSION['nombre'];
+    $apellido1 = $_SESSION['apellido1'];
+    $apellido2 = $_SESSION['apellido2'];
+    $estado_usu = $_SESSION['estado_usu'];
+    $empresas = $_SESSION['$empresas'];
+    $equipos = $_SESSION['$equipos'];
+} else {
+    session_destroy();
+    $_SESSION[] = array();
+    header("location:../index.php?error=login");
+    exit();
+}
+//endregion
+
+//region LOGICA PARA SABER CUAL ES LA PAGINA DE ORIGEN.
+// SI VENIMOS DE NOVEDADES O ACTIVAS PREPARAMOS URL PARA EL DESTINO DEL BOTON "LEER MAS TARDE"
+if ($_SESSION['webOrigen'] === 'novedades') {
+    $urlOrigen = './novedades.php';
+} elseif ($_SESSION['webOrigen'] === 'activas') {
+    $urlOrigen = './activas.php';
+} else {
+    $urlOrigen = './novedades.php';
+}
+//endregion
+
+
+// CONECTAMOS CON LA BD.
+$db_operario = new mysqli('hl793.dinaserver.com', 'gonza_currito', 'NovedadesCurrito!',
+    'gonza_novedades');
+// Comprobamos si hay error de conexión.
+if ($db_operario->connect_errno) {
+    header("location:../index.php?error=mysql");
+}
+$db_operario->set_charset('utf8mb4');
+
+// CONSULTAMOS DATOS DEL USUARIO PARA VERIFICAR.
+if (isset($sql_test_usuario)) {
+    $stmt_test_usuario = $db_operario->prepare($sql_test_usuario);
+    if ($stmt_test_usuario === false) {
+        $db_operario->close();
+        exit("No se pudo preparar SQL test_usuario");
+    }
+    // LEER DATOS USUARIO
+    $stmt_test_usuario->bind_param('i', $id_usuario);
+    $stmt_test_usuario->execute();
+    $stmt_test_usuario->bind_result($usuario, $email, $pass);
+    $res_test_usuario = $stmt_test_usuario->fetch();
+    if ($res_test_usuario === null) {
+        $stmt_test_usuario->close();
+        exit("Error en la consulta, no se recuperaron datos");
+    }
+}
+$db_operario->close();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Novedades</title>
+    <link rel="icon" href="../img/favicon.png">
+    <link rel="stylesheet" href="../css/usuario.css">
+    <link rel="stylesheet" href="../css/general-queries.css">
+    <script src="../librerias/funcionesJS.js"></script>
+</head>
+<body id="root">
+<header class="sombra0">
+    <h1 class="txt0 fs0" style="color:var(--txt-r1)">DATOS PERSONALES</h1>
+</header>
+<main class="altura0">
+    <div id="error_container"><?php error_get() ?></div>
+    <form id="introducir" method="post" action="usuarioLogic.php"></form>
+    <form id="cancelar" method="post" action="usuarioLogic.php"></form>
+    <div class="mainGrid altura0">
+        <div class="nombre justified altura1">
+            <fieldset class="left txt3 fs2">
+                <legend class="txt2 fs2">Nombre de pila</legend>
+                <p class="fs1"><?= $nombre ?></p>
+            </fieldset>
+        </div>
+        <div class="apellido1 justified altura1">
+            <fieldset class="left txt3 fs2">
+                <legend>Primer apellido</legend>
+                <p class="fs1"><?= $apellido1 ?></p>
+            </fieldset>
+        </div>
+        <div class="apellido2 justified altura1">
+            <fieldset class="left txt3 fs2">
+                <legend>Segundo apellido</legend>
+                <p class="fs1"><?= $apellido2 ?></p>
+            </fieldset>
+        </div>
+        <div class="categoria justified altura1">
+            <fieldset class="left txt3 fs2">
+                <legend>Categoría</legend>
+                <p class="fs1"><?= $nivel ?></p>
+            </fieldset>
+        </div>
+        <div class="usuario justified altura0">
+            <fieldset class="altura1 sombra1 left txt1 fs2">
+                <legend>Nombre de usuario (LOGIN)</legend>
+                <label for="usuario"></label>
+                <input class="txt1 fs1" type="text" name="usuario" id="usuario" form="introducir" maxlength="32"
+                       value="<?= $usuario ?>">
+            </fieldset>
+        </div>
+        <div class="email justified altura0">
+            <fieldset class="altura1 sombra1 left txt1 fs2">
+                <legend>Correo Electrónico</legend>
+                <label for="email"></label>
+                <input class="txt1 fs1" type="email" name="email" id="email" form="introducir" maxlength="64"
+                       value="<?= $email ?>">
+            </fieldset>
+        </div>
+        <div class="pass center altura0">
+            <fieldset class="altura1 sombra1 left txt1 fs2">
+                <legend>Nueva contraseña</legend>
+                <label for="pass"></label>
+                <input class="txt1 fs1" type="password" name="pass" id="pass" form="introducir">
+            </fieldset>
+
+        </div>
+        <div class="vpass center altura0">
+            <fieldset class="altura1 sombra1 left txt1 fs2">
+                <legend>Verificar contraseña</legend>
+                <label for="vpass"></label>
+                <input class="txt1 fs1" type="password" name="vpass" id="vpass" form="introducir">
+            </fieldset>
+        </div>
+        <div class="cancelar justified webButton">
+            <input type="hidden" name="cancelar" form="cancelar" value="<?= $urlOrigen ?>">
+            <input type="submit" id="btn_cancelar" class="webButton txt-r2 fs1" form="cancelar" value="SALIR">
+        </div>
+        <div class="actualizar justified webButton">
+            <input type="hidden" name="enviar" form="introducir" value="<?= $urlOrigen ?>">
+            <input type="submit" id="btn_enviar" class="webButton txt-r2 fs1" form="introducir" value="ACUALIZAR">
+        </div>
+    </div>
+</main>
+<footer id="mainMenu" class="sombra0f">
+    <?php
+    printMenu();
+    ?>
+</footer>
+<script>
+    botonSalir();
+</script>
+</body>
+</html>
---
+++ modulos/usuarioLogic.php
@@ -0,0 +1,116 @@
+<?php
+require_once('../librerias/consultas.php');
+require_once('../librerias/funcionesPHP.php');
+//debugFor("79.152.7.228");
+
+session_start();
+
+//region RECOGIDA VARIABLES DE SESSION TRAS LOGIN
+// DATOS RECOGIDOS EN LOGIN - SI NO ESTÁN, ACCESO NO AUTENTIFICADO -> LOGIN.PHP.
+if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], $_SESSION['apellido2'],
+    $_SESSION['estado_usu'], $_SESSION['$empresas'])) {
+    $id_usuario = $_SESSION['id_usuario'];
+    $nivel = $_SESSION['nivel'];
+    $nombre = $_SESSION['nombre'];
+    $apellido1 = $_SESSION['apellido1'];
+    $apellido2 = $_SESSION['apellido2'];
+    $estado_usu = $_SESSION['estado_usu'];
+    $empresas = $_SESSION['$empresas'];
+    $equipos = $_SESSION['$equipos'];
+} else {
+    session_destroy();
+    $_SESSION[] = array();
+    header("location:../index.php?error=login");
+    exit();
+}
+//endregion
+
+// SI LLEGA POST CANCELAR VOLVEMOS A LA PAGINA DE ORIGEN.
+if (isset($_POST['cancelar'])) {
+    $url_origen_cancelar = $_POST['cancelar'];
+    header("location:" . $url_origen_cancelar);
+    exit();
+}
+// SI LLEGA POST ENVIAR RECOGEMOS DATOS Y SEGUIMOS.
+if (isset($_POST['usuario'], $_POST['email'], $_POST['pass'], $_POST['vpass'], $_POST['enviar'])) {
+    $post_usuario = $_POST['usuario'];
+    $post_email = $_POST['email'];
+    $post_pass = $_POST['pass'];
+    $post_vpass = $_POST['vpass'];
+    $url_origen_enviar = $_POST['enviar'];
+} else {
+    header("location:novedades.php");
+    exit();
+}
+
+// VERIFICACIÓN DEL ESTADO DE LOS CAMPOS
+empty($post_usuario) ? $err_usuario = true : $err_usuario = false;
+empty($post_email) ? $err_email = true : $err_email = false;
+empty($post_pass) ? $err_pass = true : $err_pass = false;
+empty($post_vpass) ? $err_vpass = true : $err_vpass = false;
+($post_pass !== $post_vpass) ? $err_diff_pass = true : $err_diff_pass = false;
+
+// SI LAS CONTRASEÑAS NO COINCIDEN VOLVEMOS AL MENÚ.
+if ($err_diff_pass) {
+    header("location:usuario.php?error=passDiff");
+    exit();
+}
+
+// CONECTAMOS CON LA BD.
+$db_operario = new mysqli('hl793.dinaserver.com', 'gonza_currito', 'NovedadesCurrito!',
+    'gonza_novedades');
+// Comprobamos si hay error de conexión.
+if ($db_operario->connect_errno) {
+    header("location:../index.php?error=mysql");
+}
+$db_operario->set_charset('utf8mb4');
+
+// CONSULTAMOS DATOS DEL USUARIO PARA VERIFICAR Y SUSTITUIR EN CADA CASO.
+if (isset($sql_test_usuario)) {
+    $stmt_test_usuario = $db_operario->prepare($sql_test_usuario);
+    if ($stmt_test_usuario === false) {
+        $db_operario->close();
+        exit("No se pudo preparar SQL test_usuario");
+    }
+    // LEER DATOS USUARIO
+    $stmt_test_usuario->bind_param('i', $id_usuario);
+    $stmt_test_usuario->execute();
+    $stmt_test_usuario->bind_result($vUsuario, $vEmail, $vPass);
+    $res_test_usuario = $stmt_test_usuario->fetch();
+    if ($res_test_usuario === null) {
+        $stmt_test_usuario->close();
+        exit("Error en la consulta, no se recuperaron datos");
+    }
+    $stmt_test_usuario->close();
+}
+
+// CAMBIAMOS LOS DATOS DE USUARIO SOLICITADOS Y DEJAMOS EL RESTO CON LAS VERSIONES VERIFICADAS.
+if (isset($sql_usuario)) {
+    $stmt_usuario = $db_operario->prepare($sql_usuario);
+    if ($stmt_usuario === false) {
+        $db_operario->close();
+        exit("No se pudo preparar SQL usuario");
+    }
+    //TESTS Y SUSTITUCIONES
+    $err_usuario ? $usuario = $vUsuario : $usuario = $post_usuario;
+    $err_email ? $email = $vEmail : $email = $post_email;
+    if ($err_pass){
+        $pass=$vPass;
+    } else {
+        $pass=password_hash($post_pass,PASSWORD_ARGON2I);
+    }
+
+    // CAMBIO DATOS
+    $stmt_usuario->bind_param('sssi', $usuario, $email, $pass,$id_usuario);
+    $stmt_usuario->execute();
+    if ($stmt_usuario->affected_rows === 1) {
+        $id_nuevo = $stmt_usuario->insert_id;
+        $insertOK = true;
+        $stmt_usuario->close();
+    } else {
+        exit("Error al insertar la noticia");
+    }
+}
+$db_operario->close();
+header("location:usuario.php?error=datPersOk");
+exit();
