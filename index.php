1c3f9e3e72f5bc9ac849584e6f29888940e27fe3
Gonzalo Tudela <gondbt@gmail.com>
2020-11-28 19:04:46 +0100

    metas scale

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ header("Expires: 0"); // Proxies.
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
     <title>Novedades</title>
     <link rel="icon" href="img/favicon.png">
     <link rel="stylesheet" href="css/index.css">
