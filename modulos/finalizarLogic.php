d04e2e5256698ca9c44526c7e38d6b0d2c05a8cd
Gonzalo Tudela <gondbt@gmail.com>
2020-11-28 19:13:44 +0100

    se comprueba si el nivel del usuario es mayor o igual que el del autor para finalizar la noticia

--- modulos/finalizarLogic.php
+++ modulos/finalizarLogic.php
@@ -1,25 +1,28 @@
 <?php
+session_start();
 require_once('../librerias/consultas.php');
 require_once('../librerias/funcionesPHP.php');
 //debugFor("79.152.7.228");
 
-session_start();
-
 // DATOS RECOGIDOS EN LOGIN.
-if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], $_SESSION['apellido2'],
-    $_SESSION['estado_usu'], $_SESSION['$empresas'])) {
+if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], $_SESSION['estado_usu'],
+    $_SESSION['$empresas'])) {
     $id_usuario = $_SESSION['id_usuario'];
     $nivel = $_SESSION['nivel'];
     $nombre = $_SESSION['nombre'];
     $apellido1 = $_SESSION['apellido1'];
-    $apellido2 = $_SESSION['apellido2'];
+    if (isset($_SESSION['apellido2'])){
+        $apellido2 = $_SESSION['apellido2'];
+    } else {
+        $apellido2 = null;
+    }
     $estado_usu = $_SESSION['estado_usu'];
     $empresas = $_SESSION['$empresas'];
     $equipos = $_SESSION['$equipos'];
 } else {
     session_destroy();
     $_SESSION[] = array();
-    header("location:../index.php?error=login");
+    header("location:../index.php?error=sesion");
     exit();
 }
 // RECUPERO EL ULTIMO ESTADO DE WEBORIGEN EN SESSION, SI NO ES POSIBLE DETERMINO QUE VOLVAMOS A NOVEDADES.
@@ -36,31 +39,46 @@ if (isset($_POST['id_noticia'])) {
     header("location:login.php");
 }
 
-//<editor-fold desc="CONEXIÓN BD E INSERT EN TABLA LEER">
+
+//CONEXIÓN BD
 $db_operario = new mysqli('hl793.dinaserver.com', 'gonza_currito', 'NovedadesCurrito!',
     'gonza_novedades');
 // Comprobamos si hay error de conexión.
 if ($db_operario->connect_errno) {
     header("location:../index.php?error=mysql");
 }
-if (isset($sql_leer, $sql_finalizar)) {
-    $stmt_leer = $db_operario->prepare($sql_leer);
-    $stmt_leer->bind_param('ii', $id_usuario, $id_noticia);
-    $stmt_leer->execute();
-    $stmt_finalizar = $db_operario->prepare($sql_finalizar);
-    $stmt_finalizar->bind_param('i', $id_noticia);
-    $stmt_finalizar->execute();
-    if ($stmt_leer->affected_rows === 1 && $stmt_finalizar->affected_rows === 1) {
-        $stmt_leer->close();
-        $stmt_finalizar->close();
-        $db_operario->close();
-        if ($origen === 'novedades') {
-            header("location:novedades.php");
-        } elseif ($origen === 'activas') {
-            header("location:activas.php");
-        } else {
-            echo 'boom! no tengo ni idea de donde vengo!';
+
+// BUSCAR EL NIVEL DEL AUTOR DE LA NOTICIA QUE VAMOS A FINALIZAR
+if (isset($sql_nivel_autor)){
+    $stmt_nivelAutor=$db_operario->prepare($sql_nivel_autor);
+    $stmt_nivelAutor->bind_param('i', $id_noticia);
+    $stmt_nivelAutor->execute();
+    $stmt_nivelAutor->bind_result($nivelAutor);
+    $stmt_nivelAutor->fetch();
+    $stmt_nivelAutor->close();
+}
+if ($nivel>=$nivelAutor) {
+    // FINALIZAR LA NOTICIA
+    if (isset($sql_leer, $sql_finalizar)) {
+        $stmt_leer = $db_operario->prepare($sql_leer);
+        $stmt_leer->bind_param('ii', $id_usuario, $id_noticia);
+        $stmt_leer->execute();
+        $stmt_finalizar = $db_operario->prepare($sql_finalizar);
+        $stmt_finalizar->bind_param('i', $id_noticia);
+        $stmt_finalizar->execute();
+        if ($stmt_finalizar->affected_rows === 1) {
+            $stmt_leer->close();
+            $stmt_finalizar->close();
+            $db_operario->close();
+            if ($origen === 'novedades') {
+                header("location:novedades.php");
+            } elseif ($origen === 'activas') {
+                header("location:activas.php");
+            } else {
+                header("location:novedades.php");
+            }
         }
     }
+} else {
+    header('location:leer.php?error=permiso');
 }
-//</editor-fold>
