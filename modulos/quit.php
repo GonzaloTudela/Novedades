c0fce90fce759157891763fb64a28f88a535417b
Gonzalo Tudela <gondbt@gmail.com>
2020-11-28 19:02:43 +0100

    comprobación de session para abrir quit

--- modulos/quit.php
+++ modulos/quit.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
+if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], $_SESSION['estado_usu'],
+    $_SESSION['$empresas'])) {
+    $id_usuario = $_SESSION['id_usuario'];
+    $nivel = $_SESSION['nivel'];
+    $nombre = $_SESSION['nombre'];
+    $apellido1 = $_SESSION['apellido1'];
+    if (isset($_SESSION['apellido2'])){
+        $apellido2 = $_SESSION['apellido2'];
+    } else {
+        $apellido2 = null;
+    }
+    $estado_usu = $_SESSION['estado_usu'];
+    $empresas = $_SESSION['$empresas'];
+    $equipos = $_SESSION['$equipos'];
+} else {
+    session_destroy();
+    $_SESSION[] = array();
+    header("location:../index.php?error=sesion");
+    exit();
+}
 $_SESSION=array();
 if (ini_get("session.use_cookies")){
     $params=session_get_cookie_params();
