5041f8e59b37b3dc29b301beda7f2ace95f479a0
Gonzalo Tudela <gondbt@gmail.com>
2020-12-06 11:27:54 +0100

    versión preliminar buscador

--- modulos/resultados.php
+++ modulos/resultados.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], 
     $nivel = $_SESSION['nivel'];
     $nombre = $_SESSION['nombre'];
     $apellido1 = $_SESSION['apellido1'];
-    if (isset($_SESSION['apellido2'])){
+    if (isset($_SESSION['apellido2'])) {
         $apellido2 = $_SESSION['apellido2'];
     } else {
         $apellido2 = null;
@@ -30,13 +30,41 @@ if (isset($_SESSION['id_usuario'], $_SESSION['nombre'], $_SESSION['apellido1'], 
 //endregion
 
 // RECOGEMOS DATOS FORMULARIO
-if (isset($_GET['fecha_ini'],$_GET['fecha_fin'],$_GET['titulo'],$_GET['cuerpo'],$_GET['autor'])){
-    $fecha_ini=$_GET['fecha_ini'];
-    $fecha_fin=$_GET['fecha_fin'];
-    $titulo=$_GET['titulo'];
-    $cuerpo=$_GET['cuerpo'];
-    $autor=$_GET['autor'];
+if (isset($_GET['fecha_ini'], $_GET['fecha_fin'], $_GET['titulo'], $_GET['cuerpo'], $_GET['autor'])) {
+    $getFecha_ini = $_GET['fecha_ini'];
+    $getFecha_fin = $_GET['fecha_fin'];
+    $getTitulo = $_GET['titulo'];
+    $getCuerpo = $_GET['cuerpo'];
+    $getAutor = $_GET['autor'];
 }
+
+// SI LAS FECHAS ESTAN VACÍAS SUSTITUIMOS POR LA MINIMA Y MAXIMA RESPECTIVAMENTE, EN CASO CONTRARIO RECOGEMOS DATOS.
+// FECHAS MINIMAS Y MÁXIMAS EN MADRIADB 1000-01-01 hasta 9999-12-31
+empty($getFecha_ini) ? $fecha_ini = '1000-01-01' : $fecha_ini = $getFecha_ini;
+empty($getFecha_fin) ? $fecha_fin = '9999-12-31' : $fecha_ini = $getFecha_fin;
+
+// TITULO: COLOCAMOS COMODINES SI VACIO, O FORMATEAMOS LA CADENA SI EL USUARIO HA ESCRITO ALGO.
+if (empty($getTitulo)) {
+    $titulo = '%';
+} else {
+    $arrayTitulo = explode(' ', $getTitulo);
+    $titulo = '%'.implode('%', $arrayTitulo).'%';
+}
+// CUERPO: COLOCAMOS COMODINES SI VACIO, O FORMATEAMOS LA CADENA SI EL USUARIO HA ESCRITO ALGO.
+if (empty($getCuerpo)) {
+    $cuerpo = '%';
+} else {
+    $arrayCuerpo = explode(' ', $getCuerpo);
+    $cuerpo = '%'.implode('%', $arrayCuerpo).'%';
+}
+// AUTOR: COLOCAMOS COMODINES SI VACIO, O FORMATEAMOS LA CADENA SI EL USUARIO HA ESCRITO ALGO.
+if (empty($getAutor)){
+    $autor = '%';
+} else {
+    $arrayAutor = explode(' ', $getAutor, 0);
+    $autor = '%'.implode('%', $arrayAutor).'%';
+}
+
 
 //region CONEXIÓN BD Y CONSULTA DE NORMAS
 $db_operario = new mysqli('hl793.dinaserver.com', 'gonza_currito', 'NovedadesCurrito!',
@@ -54,7 +82,7 @@ if ($nivel >= 0 && $nivel <= 998) {
             $db_operario->close();
             exit('Error sentencia SQL');
         }
-        $stmt_buscar->bind_param('sssss', $fecha_ini,$fecha_fin,$titulo,$cuerpo,$autor);
+        $stmt_buscar->bind_param('sssssi', $fecha_ini, $fecha_fin, $titulo, $cuerpo, $autor, $id_usuario);
         $stmt_buscar->execute();
         $res_buscar = $stmt_buscar->get_result();
         $buscar = $res_buscar->fetch_all(MYSQLI_ASSOC);
@@ -69,7 +97,7 @@ elseif ($nivel === 999) {
             $db_operario->close();
             exit('Error sentencia SQL');
         }
-        $stmt_buscar->bind_param('sssss', $fecha_ini,$fecha_fin,$titulo,$cuerpo,$autor);
+        $stmt_buscar->bind_param('sssss', $fecha_ini, $fecha_fin, $titulo, $cuerpo, $autor);
         $stmt_buscar->execute();
         $res_buscar = $stmt_buscar->get_result();
         $buscar = $res_buscar->fetch_all(MYSQLI_ASSOC);
@@ -81,7 +109,7 @@ elseif ($nivel === 999) {
 }
 //endregion
 
-// AÑADIRMOS A SESSION LAS NORMAS --- ANTES DE CAMBIAR EL ORDEN DE LAS FECHAS ---;
+// AÑADIRMOS A SESSION LOS RESULTADOS --- ANTES DE CAMBIAR EL ORDEN DE LAS FECHAS ---;
 $_SESSION['buscar'] = $buscar;
 $_SESSION['webOrigen'] = 'buscar';
 
@@ -114,6 +142,8 @@ for ($i = 0; $i < $size; $i++) {
 <body id="root">
 <header class="sombra0">
     <h1 class="txt0 fs0" style="color:var(--txt-r1)">RESULTADOS DE LA BÚSQUEDA</h1>
+    <button class="tituloButton"><?php echo file_get_contents('../img/account.svg') ?><span><?=$nombre?></span></button>ç
+    <span class="fecha"><?php echo date('d/m/Y')?></span>
 </header>
 <main class="altura0">
     <div class="mainGrid altura0">
